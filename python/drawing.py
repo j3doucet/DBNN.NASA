86aac746317d703fe23fc4430986cbcc0c04e7d8
John Doucette <jaedoucette@gmail.com>
2015-04-12 01:47:33 +0000

    An awful not threadsafe drawing thing, but with nicer graphics.

--- python/drawing.py
+++ python/drawing.py
@@ -2,38 +2,68 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""
-ZetCode PySide tutorial 
-
-This example draws three rectangles in three
-different colors. 
-
-author: Jan Bodnar
-website: zetcode.com 
-last edited: August 2011
-"""
 
 import time
+import threading
 import sys
 import random
+import re
 from math import exp
 from PySide import QtGui, QtCore
+import sys
 
 class Example(QtGui.QWidget):
-    
+
+    def getWeights(pythonisdumbdumb,x):
+        f = open("./../Data/Model.NN","r")
+        lines = f.readlines();
+
+        matrices = []
+        matrix = []
+        for line in lines:
+            row = []
+            result = re.match("(\d+)\s+(\d+)\s*\n", line)
+            if(result):
+                #rows = result.group[0] 
+                #cols = result.group[1]
+                if(len(matrix) != 0):
+                    matrices.append(matrix)
+                    matrix = []
+            else:
+                row = line.split()
+                matrix.append(row)
+        matrices.append(matrix)
+        return matrices[x]
+
+
+    lock = threading.Lock()
+    currentData = [-10, -5, 5, 10, -1, 0, 0, 1]
+    def getData(self, tehdataz):
+        yield from lock
+        try:
+            self.currentData = tehdataz
+        finally:
+            lock.release()
+
     def __init__(self):
         super(Example, self).__init__()
-        
+
         self.initUI()
-        
+
     def initUI(self):      
 
+        print("hello\n")
+        sys.stdout.flush
         self.setGeometry(300, 300, 280, 270)
         self.setWindowTitle('Brain')
         self.show()
+        print("hello\n")
+        sys.stdout.flush
 
 
     def paintEvent(self, e):
+        print("hello\n")
+        sys.stdout.flush
 
         qp = QtGui.QPainter()
         qp.begin(self)
@@ -43,37 +73,46 @@ class Example(QtGui.QWidget):
             yinit = 30
             xspace = 150
             yspace = 60
-            for i in range(1,9):
-                for j in range(1,9):
-                    self.ConnectNodes(qp,xinit, yinit+i*yspace, xinit+xspace, yinit+j*yspace, 0) 
-                self.drawNode(qp,xinit, yinit+i*yspace, 0)
+            matrix = self.getWeights(1)
+            mysum = []
+            print("hello\n")
+            sys.stdout.flush
 
-            xinit = xinit + xspace 
             for i in range(1,9):
+                localsum =0 
                 for j in range(2,8):
-                    self.ConnectNodes(qp,xinit, yinit+i*yspace, xinit+xspace, yinit+j*yspace, 0) 
-                self.drawNode(qp,xinit, yinit+i*yspace, 0)
+                    self.ConnectNodes(qp,xinit, yinit+i*yspace, xinit+xspace, yinit+j*yspace, matrix[i-1][j-2]) 
+                    localsum += self.currentData[i-1]*float(matrix[i-1][j-2])
+                mysum.append(localsum)
+                self.drawNode(qp,xinit, yinit+i*yspace, 0, self.currentData[i-1])
 
+
+            matrix = self.getWeights(2)
             xinit = xinit + xspace
+            nextsum = []
             for i in range(2,8):
+                localsum =0 
                 for j in range(3,8):
-                    self.ConnectNodes(qp,xinit, yinit+i*yspace, xinit+xspace, yinit+j*yspace-yspace/2, 0) 
-                self.drawNode(qp,xinit, yinit+i*yspace, 0)
+                    self.ConnectNodes(qp,xinit, yinit+i*yspace, xinit+xspace, yinit+j*yspace-yspace/2, matrix[i-2][j-3]) 
+                    localsum += mysum[i-2]*float(matrix[i-2][j-3])
+                nextsum.append(localsum)
+                self.drawNode(qp,xinit, yinit+i*yspace, 0, mysum[i-2])
 
             xinit = xinit + xspace
             for i in range(2,7):
-                self.drawNode(qp,xinit, yinit+i*yspace + yspace/2, 1)
+                self.drawNode(qp,xinit, yinit+i*yspace + yspace/2, 1, nextsum[i-2])
         qp.end()
 
-    def drawNode(self, qp, x, y, isend):
+    def drawNode(self, qp, x, y, isend, colorlevel):
+        if(colorlevel < -10):
+            colorlevel = -10
+        redLev = 1.0/(1.0+exp(-colorlevel))
+        blueLev = 1.0/(1.0+exp(colorlevel))
         pen = QtGui.QPen(QtCore.Qt.black, 2, QtCore.Qt.SolidLine)
         gradient = QtGui.QRadialGradient(x, y, 20, x,y)
-        if(random.random() < 0.25):
-            gradient.setColorAt(0, QtGui.QColor.fromRgbF(0, 1, 0, 1))
-        else:
-            gradient.setColorAt(0, QtGui.QColor.fromRgbF(1, 0, 0, 1))
+        gradient.setColorAt(0, QtGui.QColor.fromRgbF(redLev, 0, blueLev, 1))
         if(isend == 1):
-            gradient.setColorAt(0, QtGui.QColor.fromRgbF(0, 0, random.random(),1))
+            gradient.setColorAt(0, QtGui.QColor.fromRgbF(blueLev, redLev, 0,1))
 
 
         gradient.setColorAt(1, QtGui.QColor.fromRgbF(0, 0, 0, 0.8))
@@ -82,16 +121,19 @@ class Example(QtGui.QWidget):
         qp.setPen(pen)
         qp.drawEllipse(QtCore.QPointF(x,y),20,20);
 
-    
+
     def ConnectNodes(self, qp, x1, y1, x2, y2, w):
-        pen = QtGui.QPen(QtCore.Qt.black, 8*1.0/(1.0+exp(-w)), QtCore.Qt.SolidLine)
+        w = float(w)
+        if(w < -10):
+            w = 10
+        pen = QtGui.QPen(QtCore.Qt.black, 8*1.0/(1.0+exp(-float(w))), QtCore.Qt.SolidLine)
         qp.setPen(pen)
 
         qp.drawLine(x1,y1,x2,y2)
 
 
 def main():
-    
+
     app = QtGui.QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
